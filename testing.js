ca7075b807face9e5608b88971052310a827bd85
Abaid Ullah Aleem <103155286+Abaid78@users.noreply.github.com>
2024-04-13 02:23:39 +0500

    Update Notes with APIs

--- testing.js
+++ testing.js
@@ -1,16 +1,28 @@
-let start=1;
-let end=1000;
-for(var i=start;i<=end;i++){
-    var temp=i;
-    var noOfDigits=i.toString().length;
-    var sum=0;
-    while(temp>0){
-        var digit=temp%10;
-        temp=parseInt(temp/10);
-        sum+=digit**noOfDigits;
-
-    }
-    if(sum==i){
-        console.log(i)
-    }
+async function hello(){
+    console.log("hello")
 }
+// this function return always Promise compulsory because of async
+
+
+//this function is created by the API we just resolve it
+function getData(dataID){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data",dataID);
+            resolve("success");
+        },2000);
+    });
+}
+
+//async-await
+async function getAllData(){
+    console.log("Getting data 1 ....");
+    await getData(1);
+    console.log("Getting data 2 .....");
+    await getData(1);
+    console.log("Getting data 3 ....");
+    await getData(1);
+
+}
+
+//await keyword used just in the async function. not used namely.
